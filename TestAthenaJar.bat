c0ec4bf73885795fc680257ae86d105bed21568a
Daniel R. Collins ("Delta") <dcollins@superdan.net>
2020-04-24 21:41:06 -0400

    Add project to make ZIP package & test

---
+++ TestAthenaJar.bat
@@ -0,0 +1,9 @@
+REM Test the Athena JAR program
+java -jar Athena.jar
+java -jar Athena.jar Arena
+java -jar Athena.jar Marshal bandits
+java -jar Athena.jar MonsterMetrics troll -f=100
+java -jar Athena.jar NPCGenerator
+java -jar Athena.jar NPCGenerator -pdf
+java -jar Athena.jar FooBar
+pause
