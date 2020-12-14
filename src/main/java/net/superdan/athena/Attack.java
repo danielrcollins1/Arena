3b62bce0daf3e18e1fddca2feea5ec31e7a41b39
Joshua Macy <joshua.macy@gmail.com>
2020-12-13 21:06:38 -0500

    Refactor Attack to add unit tests

--- src/main/java/net/superdan/athena/Attack.java
+++ src/main/java/net/superdan/athena/Attack.java
@@ -107,19 +107,9 @@ public class Attack {
      */
     public String toString() {
         return (rate == 1 ? "" : rate + " ")
-                + (name == null ? "net.superdan.athena.Attack" : name) + " "
+                + (name == null ? "Attack" : name) + " "
                 + Dice.formatBonus(bonus)
                 + " (" + damage + ")";
-    }
-
-    /**
-     * Main test function.
-     */
-    public static void main(String[] args) {
-        Attack atk = new Attack(2, 1);
-        System.out.println(atk);
-        atk = new Attack("Claw", 2, 6, new Dice(1, 6));
-        System.out.println(atk);
     }
 }
 
